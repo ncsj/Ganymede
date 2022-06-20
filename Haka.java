81f2f3b56c5fa85a94d09e146efe008e5b1efffe
RyotaYamagucchi <j22105@ncs-j.jp>
2022-06-20 21:19:17 +0900

    sfv

--- Haka.java
+++ Haka.java
@@ -2,6 +2,6 @@ public class Haka{
     public static void main(Stirng args[]){
         System.out.println("!");
         System.out.println("d");
-        System.out.println("f");
+        System.out.println("sdkbcsdh");
     }    
 }
