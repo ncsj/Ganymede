1c27bcbab461d31721e4f2032bdf5900f6ccc40f
RyotaYamagucchi <107821389+RyotaYamagucchi@users.noreply.github.com>
2022-06-21 15:18:46 +0900

    Delete Haka.java

--- Haka.java
+++
@@ -1,7 +0,0 @@
-public class Haka{
-    public static void main(Stirng args[]){
-        System.out.println("!");
-        System.out.println("d");
-        System.out.println("sdkbcsdh");
-    }    
-}
