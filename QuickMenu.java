e309da02706a8ebff05acc45718c131e42e9c568
msncsj <j22103@ncs-j.jp>
2022-06-21 22:57:54 +0900

    ver.03

--- QuickMenu.java
+++ QuickMenu.java
@@ -9,13 +9,17 @@ public class QuickMenu extends Frame{
     Button btn2  = new Button("QR読み取り");
     Button btn3  = new Button("観覧エリアを写真から選択");
     Button ebtn  = new Button("EXIT");
+    Button btn4  = new Button("コメントを見る");
+    Button btn5  = new Button("コメントを投稿する");
+    Button btn6  = new Button ("観覧エリアを要素から選択");
+
     MenuBar mbar = new MenuBar();
     Label label1  = new Label("QUICK MENU"); 
     Label label2  = new Label("お知らせ");
 
-    Menu	regiMenu	= new Menu("登録");
-	Menu	listMenu	= new Menu("リスト");
-	Menu	watchMenu	= new Menu("どこで見る？？");
+    Menu	regiMenu	= new Menu("登録する");
+	Menu	listMenu	= new Menu("情報を見る");
+	Menu	watchMenu	= new Menu("どこで観覧する？？");
 
 	String [] regiMenuItems = {"バーコード読み取り","-","コメント登録"};
 	String [] listMenuItems = {"混雑状況","-","コメントリスト"};
@@ -36,22 +40,31 @@ public class QuickMenu extends Frame{
 
     public void initmenu(){
         add(btn1);
-        btn1.setBounds(140,200,250,150);
+        btn1.setBounds(140,100,250,150);
         
         add(btn2);
-        btn2.setBounds(490,200,250,150);
+        btn2.setBounds(490,100,250,150);
 
         add(btn3);
-        btn3.setBounds(840,200,250,150);
+        btn3.setBounds(840,100,250,150);
 
         add(olist);
         olist.setBounds(140,450,950,300);
+
+        add(btn4);
+        btn4.setBounds(140,270,250,150);
+
+        add(btn5);
+        btn5.setBounds(490,270,250,150);
+
+        add(btn6);
+        btn6.setBounds(840,270,250,150);
 
         add(ebtn);
         ebtn.setBounds(990,760,100,20);
 
         add(label1);
-        label1.setBounds(20,50,100,20);
+        label1.setBounds(20,60,100,20);
 
         add(label2);
         label2.setBounds(140,430,100,20);
